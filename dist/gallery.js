c4a6cb3916226821c03233c434eb4da3022a1417
Nico Bernold <nb@feinheit.ch>
2017-05-12 14:40:53 +0200

    adding get & set for autoplay, adding creation of thumbs inside gallery

--- dist/gallery.js
+++ dist/gallery.js
@@ -36,82 +36,41 @@ function applyTransform(element, transform) {
 
 var Gallery = exports.Gallery = function () {
   function Gallery(element, options) {
-    var _this = this;
-
     _classCallCheck(this, Gallery);
 
     this.element = element;
     this.slider = $('[data-slider]', this.element);
     this.slides = $.$('[data-slide]', this.element);
-    this.thumbs = $.$('[data-thumb]', this.element);
+    this.thumbsContainer = $('[data-thumbs]', this.element);
+    this.playPause = $('[data-playpause]', this.element);
 
     this.options = $.extend({
       interval: 5000,
-      autoPlay: false
+      autoPlay: true,
+      createThumbs: true
     }, options);
 
     this._current = null;
     this._interval = null;
 
     this._setWidthsAndPositions();
+
+    this.options.createThumbs && this._createThumbs();
+
+    this.thumbs = $.$('[data-thumb]', this.element);
+
+    this._setEventListeners();
+
     this.reveal(0);
 
-    this.thumbs.forEach(function (thumb, index) {
-      thumb.addEventListener('click', function (e) {
-        e.preventDefault();
-        _this.autoPlay(false);
-        _this.reveal(index);
-      });
-    });
-
-    // add 'click' to left and right arrow
-    $.$('[data-go]', this.element)._.addEventListener('click', function (e) {
-      e.preventDefault();
-      _this.autoPlay(false);
-      _this.reveal(_this._current + parseInt(e.currentTarget.dataset.go, 10));
-    });
-
-    var events = new _swipedetector.SwipeDetector(this.element).emitter;
-    events.on('left', function () {
-      _this.autoPlay(false);
-      _this.reveal(_this._current + 1);
-    });
-    events.on('right', function () {
-      _this.autoPlay(false);
-      _this.reveal(_this._current - 1);
-    });
-
-    window.addEventListener('resize', function () {
-      _this._setWidthsAndPositions();
-      _this.reveal(_this._current);
-    });
-
-    if (this.options.autoPlay) {
-      this.autoPlay(true);
-    }
+    this.autoPlay = this.options.autoPlay;
   }
 
   _createClass(Gallery, [{
     key: 'move',
     value: function move(direction) {
-      this.autoPlay(false);
+      this.autoPlay = false;
       this.reveal(this._current + direction);
-    }
-  }, {
-    key: 'autoPlay',
-    value: function autoPlay(enable) {
-      var _this2 = this;
-
-      if (enable) {
-        if (!this._interval) {
-          this._interval = setInterval(function () {
-            return _this2.reveal(_this2._current + 1);
-          }, this.options.interval);
-        }
-      } else if (this._interval) {
-        clearInterval(this._interval);
-        this._interval = null;
-      }
     }
   }, {
     key: 'reveal',
@@ -124,13 +83,95 @@ var Gallery = exports.Gallery = function () {
   }, {
     key: '_setWidthsAndPositions',
     value: function _setWidthsAndPositions() {
-      var _this3 = this;
+      var _this = this;
 
       this.width = parseFloat(getComputedStyle(this.element).getPropertyValue('width'));
       this.slider.style.width = this.width * this.slides.length + 'px';
       this.slides.forEach(function (slide) {
-        slide.style.width = _this3.width + 'px';
+        slide.style.width = _this.width + 'px';
       });
+    }
+  }, {
+    key: '_setEventListeners',
+    value: function _setEventListeners() {
+      var _this2 = this;
+
+      this.thumbs.forEach(function (thumb, index) {
+        thumb.addEventListener('click', function (e) {
+          e.preventDefault();
+          _this2.autoPlay = false;
+          _this2.reveal(index);
+        });
+      });
+
+      // add 'click' to left and right arrow
+      $.$('[data-go]', this.element)._.addEventListener('click', function (e) {
+        e.preventDefault();
+        _this2.autoPlay = false;
+        _this2.reveal(_this2._current + parseInt(e.currentTarget.dataset.go, 10));
+      });
+
+      this.playPause && this.playPause.addEventListener('click', function (e) {
+        e.preventDefault();
+        _this2.autoPlay = !_this2.autoPlay;
+      });
+
+      var events = new _swipedetector.SwipeDetector(this.element).emitter;
+      events.on('left', function () {
+        _this2.autoPlay = false;
+        _this2.reveal(_this2._current + 1);
+      });
+      events.on('right', function () {
+        _this2.autoPlay = false;
+        _this2.reveal(_this2._current - 1);
+      });
+
+      window.addEventListener('resize', function () {
+        _this2._setWidthsAndPositions();
+        _this2.reveal(_this2._current);
+      });
+    }
+  }, {
+    key: '_createThumbs',
+    value: function _createThumbs() {
+      var _this3 = this;
+
+      if (this.thumbsContainer) {
+        if (this.slides.length > 1) {
+          this.slides.forEach(function () {
+            _this3.thumbsContainer.appendChild($.create({
+              tag: 'a',
+              href: '',
+              'data-thumb': ''
+            }));
+          });
+        }
+      } else {
+        console.warn('bliss-gallery: no element with [data-thumbs] found.');
+      }
+    }
+  }, {
+    key: 'autoPlay',
+    get: function get() {
+      return this._interval != null;
+    },
+    set: function set(enable) {
+      var _this4 = this;
+
+      if (enable) {
+        this.element.dataset.playing = true;
+        if (!this._interval) {
+          this._interval = setInterval(function () {
+            return _this4.reveal(_this4._current + 1);
+          }, this.options.interval);
+        }
+      } else {
+        this.element.dataset.playing = false;
+        if (this._interval) {
+          clearInterval(this._interval);
+          this._interval = null;
+        }
+      }
     }
   }]);
 
--- src/gallery.js
+++ src/gallery.js
@@ -27,72 +27,56 @@ export class Gallery {
     this.element = element;
     this.slider = $('[data-slider]', this.element);
     this.slides = $.$('[data-slide]', this.element);
-    this.thumbs = $.$('[data-thumb]', this.element);
+    this.thumbsContainer = $('[data-thumbs]', this.element);
+    this.playPause = $('[data-playpause]', this.element);
 
     this.options = $.extend({
       interval: 5000,
-      autoPlay: false,
+      autoPlay: true,
+      createThumbs: true,
     }, options);
 
     this._current = null;
     this._interval = null;
 
     this._setWidthsAndPositions();
+
+    this.options.createThumbs && this._createThumbs();
+
+    this.thumbs = $.$('[data-thumb]', this.element);
+
+    this._setEventListeners();
+
     this.reveal(0);
 
-    this.thumbs.forEach((thumb, index) => {
-      thumb.addEventListener('click', (e) => {
-        e.preventDefault();
-        this.autoPlay(false);
-        this.reveal(index);
-      });
-    });
-
-    // add 'click' to left and right arrow
-    $.$('[data-go]', this.element)._.addEventListener('click', (e) => {
-      e.preventDefault();
-      this.autoPlay(false);
-      this.reveal(this._current + parseInt(e.currentTarget.dataset.go, 10));
-    });
-
-    let events = new SwipeDetector(this.element).emitter;
-    events.on('left', () => {
-      this.autoPlay(false);
-      this.reveal(this._current + 1);
-    });
-    events.on('right', () => {
-      this.autoPlay(false);
-      this.reveal(this._current - 1);
-    });
-
-    window.addEventListener('resize', () => {
-      this._setWidthsAndPositions();
-      this.reveal(this._current);
-    });
-
-    if(this.options.autoPlay) {
-      this.autoPlay(true);
-    }
+    this.autoPlay = this.options.autoPlay;
   }
 
   move(direction) {
-    this.autoPlay(false);
+    this.autoPlay = false;
     this.reveal(this._current + direction);
   }
 
-  autoPlay(enable) {
+  get autoPlay() {
+    return (this._interval != null);
+  }
+
+  set autoPlay(enable) {
     if (enable) {
+      this.element.dataset.playing = true;
       if (!this._interval) {
         this._interval = setInterval(
           () => this.reveal(this._current + 1),
           this.options.interval);
       }
-    } else if(this._interval) {
-      clearInterval(this._interval);
-      this._interval = null;
+    } else {
+      this.element.dataset.playing = false;
+      if(this._interval) {
+       clearInterval(this._interval);
+       this._interval = null;
+     }
     }
   }
-
 
   reveal(index) {
     this.thumbs[this._current] && this.thumbs[this._current].removeAttribute('data-current');
@@ -107,5 +91,59 @@ export class Gallery {
     this.slides.forEach(slide => {
       slide.style.width = `${this.width}px`;
     });
+  }
+
+  _setEventListeners() {
+    this.thumbs.forEach((thumb, index) => {
+      thumb.addEventListener('click', (e) => {
+        e.preventDefault();
+        this.autoPlay = false;
+        this.reveal(index);
+      });
+    });
+
+    // add 'click' to left and right arrow
+    $.$('[data-go]', this.element)._.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.autoPlay = false;
+      this.reveal(this._current + parseInt(e.currentTarget.dataset.go, 10));
+    });
+
+    this.playPause && this.playPause.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.autoPlay = !this.autoPlay;
+    });
+
+    let events = new SwipeDetector(this.element).emitter;
+    events.on('left', () => {
+      this.autoPlay = false;
+      this.reveal(this._current + 1);
+    });
+    events.on('right', () => {
+      this.autoPlay = false;
+      this.reveal(this._current - 1);
+    });
+
+    window.addEventListener('resize', () => {
+      this._setWidthsAndPositions();
+      this.reveal(this._current);
+    });
+  }
+
+  _createThumbs() {
+    if (this.thumbsContainer) {
+      if (this.slides.length > 1) {
+        this.slides.forEach(() => {
+          this.thumbsContainer.appendChild($.create({
+            tag: 'a',
+            href: '',
+            'data-thumb': '',
+          }));
+        });
+      }
+    } else {
+      console.warn('bliss-gallery: no element with [data-thumbs] found.');
+    }
+
   }
 }
